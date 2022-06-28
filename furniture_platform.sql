048667aace0093c6d94604cec2f7d3180e78d1e0
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-06-28 17:03:17 +0200

    DB created

---
+++ furniture_platform.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 28, 2022 at 10:13 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `furniture_platform`
+--
+
+CREATE DATABASE IF NOT EXISTS `furniture_platform` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `furniture_platform`;
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meubles`
+--
+
+DROP TABLE IF EXISTS `meubles`;
+CREATE TABLE `meubles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `prix` varchar(255) NOT NULL,
+  `hauteur` int(10) UNSIGNED NOT NULL,
+  `largeur` int(10) UNSIGNED NOT NULL,
+  `profondeur` int(10) UNSIGNED NOT NULL,
+  `matiere` varchar(255) NOT NULL,
+  `couleur` varchar(255) NOT NULL,
+  `photo1` varchar(255) NOT NULL,
+  `photo2` varchar(255) NOT NULL,
+  `photo3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+CREATE TABLE `personne` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `mot de passe` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendeur`
+--
+
+DROP TABLE IF EXISTS `vendeur`;
+CREATE TABLE `vendeur` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `personne_id` int(10) NOT NULL,
+  `meubles_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `vendeur`
+--
+
+DROP TABLE IF EXISTS `acheteur`;
+CREATE TABLE `acheteur` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `personne_id` int(10) NOT NULL,
+  `meubles_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `meubles`
+--
+ALTER TABLE `meubles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vendeur`
+--
+ALTER TABLE `vendeur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_personne_vendeur_id` (`personne_id`),
+  ADD KEY `fk_meubles_vendeur_id` (`meubles_id`);
+
+ALTER TABLE `acheteur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_personne_acheteur_id` (`personne_id`),
+  ADD KEY `fk_meubles_acheteur_id` (`meubles_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `meubles`
+--
+ALTER TABLE `meubles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `acheteur`
+--
+ALTER TABLE `acheteur`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vendeur`
+--
+ALTER TABLE `vendeur`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+
+
+-- AJOUT FOREIGN KEYS POUR LIER LES TABLES  
+
+ALTER TABLE `vendeur` 
+  ADD CONSTRAINT `fk_personne_vendeur_id` FOREIGN KEY (`personne_id`) 
+  REFERENCES `personne`(`id`) 
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `vendeur` 
+  ADD CONSTRAINT `fk_meubles_vendeur_id` FOREIGN KEY (`meubles_id`) 
+  REFERENCES `meubles`(`id`)  
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `acheteur` 
+  ADD CONSTRAINT `fk_personne_acheteur_id` FOREIGN KEY (`personne_id`) 
+  REFERENCES `personne`(`id`)
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `acheteur` 
+  ADD CONSTRAINT `fk_meubles_acheteur_id` FOREIGN KEY (`meubles_id`) 
+  REFERENCES `meubles`(`id`)  
+  ON DELETE NO ACTION 
+  ON UPDATE NO ACTION;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
